6a06a32705e55dfdfd0ae323a6e29a60029bf95f
Mateus0808 <loiolamateus7@gmail.com>
2024-11-01 14:31:21 -0300

    feat: supermercado

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+## Randoop
+```
+java -cp ./randoop-all-4.3.3.jar:target/classes randoop.main.Main gentests --testclass=com.adamant.Cliente --output-limit=100 --junit-output-dir=./src/test/java
+```
---
+++ src/main/java/com/adamant/EnumTipoDePagamento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adamant;
+
+/**
+ *
+ * @author Richiely Batista, Filipe Maciel
+ */
+public enum EnumTipoDePagamento {
+    DINHEIRO, CARTAO;
+}
